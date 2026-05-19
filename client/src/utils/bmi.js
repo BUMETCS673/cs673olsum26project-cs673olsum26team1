7850c5c355fa21b18b4a1875607af3b9109b4cd0
kasch890 <kai.fernandes890@gmail.com>
2026-05-19 09:03:05 -0400

    [HYBRID] Add BMI calculator function and created components and utils folders

--- client/src/utils/bmi.js
+++ client/src/utils/bmi.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {*} height - height in inches
+ * @param {*} weight - weight in pounds
+ * @returns BMI
+ */
 export function calculateBMI(height, weight){
     if (height <= 0 || weight <= 0) {
         throw new Error("Height and weight must be greater than zero.");
