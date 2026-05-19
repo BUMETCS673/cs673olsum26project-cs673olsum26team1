1b557e38c40fb2455e9bea8d453e62ef3fd45a80
kasch890 <kai.fernandes890@gmail.com>
2026-05-19 09:09:57 -0400

    [HYBRID]Integrate BMI calculator into patient portal

--- client/src/pages/PatientPortal.jsx
+++ client/src/pages/PatientPortal.jsx
@@ -1,7 +1,10 @@
+import BMICalculator from "../components/BMICalculator";
+
 const PatientPortal = () => {
   return (
     <div className="container mt-5">
       <h1>Patient Portal</h1>
+      <BMICalculator />
     </div>
   );
 };
